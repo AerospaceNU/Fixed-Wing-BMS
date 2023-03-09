b1c7b849c4c9d34e5cd107a127d02c9f3defff02
BenVeghte <Benveghte@live.com>
2023-03-08 22:36:30 -0500

    Add pin definitions

--- Teensy-DC1942C-Testing/Teensy-DC1942C-Testing.ino
+++ Teensy-DC1942C-Testing/Teensy-DC1942C-Testing.ino
@@ -1,14 +1,24 @@
+/***Libraries****/
 #include <LTC68042.h>
-
 #include <UserInterface.h>
-
 #include <Linduino.h>
-
 #include <LT_SPI.h>
+
+/***Definitions***/
+//Pin Definitions
+#define CS_PIN 10 //This is likely unnessary as arduino likely autoassigns SS to 10. See https://www.pjrc.com/teensy/td_libs_SPI.html the pin definition for LTC6804_CS
+#define MOSI_PIN 11
+#define MISO_PIN 12
+#define SCK_PIN 13
+
+//Other Definitions
+#define TOTAL_ICs 1 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600)
+  while(! Serial); //Waits until Serial is available, we might not want this down the line. 
+
 }
 
 void loop() {
